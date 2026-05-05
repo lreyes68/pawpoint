b1a86bbf77b5dac6404cbe972f8dcdd6a4dbd4e0
Leandro Reyes <leandroreyes3548@gmail.com>
2026-05-04 18:11:49 -0700

    Added marker when user clicks, has lat and lang coords.

--- static/game.js
+++ static/game.js
@@ -3,7 +3,22 @@
 // https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png
 // https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png
 
-var map = L.map('map').setView([37.3644754,-120.4231159], 16);
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    maxZoom: 17,
-}).addTo(map);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var map = L.map('map').setView([37.3644754,-120.4231159], 16);
+    var currentMarker
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        maxZoom: 20,
+    }).addTo(map);
+
+    function onMapClick(e) {
+        if(currentMarker) {
+            map.removeLayer(currentMarker)
+        }
+
+        currentMarker = L.marker(e.latlng).addTo(map)
+    }
+
+    map.on('click', onMapClick);
+})
